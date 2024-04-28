f569cdfb7e3d06adadf115414eda641203b7fa47
Chris Pyles <cpyles@berkeley.edu>
2024-04-28 16:30:59 -0700

    fixing base href issues

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
+import { APP_BASE_HREF } from '@angular/common';
 import { MessageService } from 'primeng/api';
 import { routes } from './app.routes';
 
@@ -10,5 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withComponentInputBinding()),
     importProvidersFrom([BrowserAnimationsModule]),
     MessageService,
+    { provide: APP_BASE_HREF, useValue: '/contrastinator' },
   ]
 };
