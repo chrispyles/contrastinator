c1335fe0d036e1c20af8ee7c279d850f2b44bb31
Chris Pyles <cpyles@berkeley.edu>
2024-04-30 20:26:19 -0700

    tests for contrast chart

--- src/app/contrast-chart/contrast-chart.component.spec.ts
+++ src/app/contrast-chart/contrast-chart.component.spec.ts
@@ -1,25 +1,77 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import Color from 'color';
 
+import { By } from '@angular/platform-browser';
 import { ContrastChartComponent } from './contrast-chart.component';
 
 describe('ContrastChartComponent', () => {
-  let component: ContrastChartComponent;
   let fixture: ComponentFixture<ContrastChartComponent>;
+  let de: DebugElement;
+  let colors: Color[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ContrastChartComponent]
-    })
-      .compileComponents();
+    }).compileComponents();
+
+    colors = [
+      new Color('#000'),
+      new Color('#223344'),
+      new Color('#FFF'),
+    ];
 
     fixture = TestBed.createComponent(ContrastChartComponent);
-    component = fixture.componentInstance;
-    const componentRef = fixture.componentRef;
-    componentRef.setInput('colors', []);
+    fixture.componentRef.setInput('colors', colors);
     fixture.detectChanges();
+
+    de = fixture.debugElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('shows a table containing the contrasts', () => {
+    expect(de.query(By.css('table'))).toBeDefined();
+
+    const cells = de.queryAll(By.css('td, th'));
+    expect(cells.map(c => c.nativeElement.innerText)).toEqual([
+      // row 1
+      '',
+      '#000000',
+      '#223344',
+      '#FFFFFF',
+
+      // row 2
+      '#FFFFFF',
+      '21',
+      '12.923',
+      '1',
+
+      // row 3
+      '#223344',
+      '1.625',
+      '1',
+      '',
+
+      // row 4
+      '#000000',
+      '1',
+      '',
+      '',
+    ]);
+  });
+
+  it('sets the background color of each th element', () => {
+    const ths = de.queryAll(By.css('th'));
+    expect(ths.map(e => {
+      const bgc = e.nativeElement.style.backgroundColor;
+      return bgc && new Color(bgc).hex();
+    })).toEqual([
+      '',
+      '#000000',
+      '#223344',
+      '#FFFFFF',
+      '#FFFFFF',
+      '#223344',
+      '#000000',
+    ]);
   });
 });
